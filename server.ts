457e3f6d51e3e80c6be7effb16f56bcaec582196
Saideep Vollala <saideepvollala@Saideeps-MacBook-Air.local>
2025-01-13 22:02:51 -0800

    Initial commit with TypeScript and ESLint setup

---
+++ server.ts
@@ -0,0 +1,18 @@
+import * as http from 'http';
+import * as fs from 'fs';
+
+const server = http.createServer((req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
